5c6af81de4f197382f1130e916e40b8e8f1ac432
DamienA <alves.d24@laposte.net>
2025-08-21 11:18:34 +0200

    refactor(layout): update fragment home

--- app/src/main/java/com/quizocr/vitesse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/quizocr/vitesse/ui/home/HomeFragment.kt
@@ -1,4 +1,24 @@
 package com.quizocr.vitesse.ui.home
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.quizocr.vitesse.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
