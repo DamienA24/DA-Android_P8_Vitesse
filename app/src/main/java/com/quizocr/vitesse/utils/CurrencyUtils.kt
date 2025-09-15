ef5cb4c932ced8ea4780f0ef37cebbf587576da0
DamienA <alves.d24@laposte.net>
2025-09-15 10:54:07 +0200

    feat(utils): add utils to dates and currency

--- app/src/main/java/com/quizocr/vitesse/utils/CurrencyUtils.kt
+++ app/src/main/java/com/quizocr/vitesse/utils/CurrencyUtils.kt
@@ -1,4 +1,12 @@
 package com.quizocr.vitesse.utils
 
-class Currencu {
+import android.icu.text.NumberFormat
+import java.util.Locale
+
+fun formatCurrencyInUk(amount: Double): String {
+    return try {
+        NumberFormat.getCurrencyInstance(Locale.UK).format(amount)
+    } catch (e: Exception) {
+        amount.toString() // Fallback
+    }
 }
--- app/src/main/java/com/quizocr/vitesse/utils/DateUtils.kt
+++ app/src/main/java/com/quizocr/vitesse/utils/DateUtils.kt
@@ -1,4 +1,28 @@
 package com.quizocr.vitesse.utils
 
-class DateUtils {
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.ChronoUnit
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun formatDateShort(date: LocalDate): String {
+    val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+    return date.format(dateFormatter)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun calculateAgeInYears(dateOfBirth: LocalDate): Long {
+    val today = LocalDate.now()
+    return ChronoUnit.YEARS.between(dateOfBirth, today)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getFormattedBirthDateAndAge(dateOfBirth: LocalDate): Pair<String, Long> {
+    val formattedDate = formatDateShort(dateOfBirth)
+    val age = calculateAgeInYears(dateOfBirth)
+    return Pair(formattedDate, age)
 }
