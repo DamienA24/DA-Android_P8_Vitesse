87da1828026773292fced6cb37ed6e2c3612c7e7
DamienA <alves.d24@laposte.net>
2025-09-15 10:54:51 +0200

    chore(di): add config for use request

--- app/src/main/java/com/quizocr/vitesse/di/NetworkModule.kt
+++ app/src/main/java/com/quizocr/vitesse/di/NetworkModule.kt
@@ -1,4 +1,48 @@
 package com.quizocr.vitesse.di
 
+import com.quizocr.vitesse.data.api.CurrencyApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val CURRENCY_API_BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/"
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+    @Provides    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(CURRENCY_API_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyApiService(retrofit: Retrofit): CurrencyApiService {
+        return retrofit.create(CurrencyApiService::class.java)
+    }
 }
--- app/src/main/java/com/quizocr/vitesse/di/RepositoryModule.kt
+++ app/src/main/java/com/quizocr/vitesse/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package com.quizocr.vitesse.di
 
-class RepositoryModule {
+import com.quizocr.vitesse.data.repository.CurrencyConversionRepository
+import com.quizocr.vitesse.data.repository.service.ExchangeRateApi
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCurrencyConversionRepository(
+        currencyConversionRepository: CurrencyConversionRepository
+    ): ExchangeRateApi
 }
