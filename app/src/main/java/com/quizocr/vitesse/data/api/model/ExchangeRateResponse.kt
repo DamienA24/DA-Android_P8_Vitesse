81f0e4bf5ef77e7c5741b24f9f62c50aafa2686a
DamienA <alves.d24@laposte.net>
2025-09-15 10:50:31 +0200

    feat(model): add class ExchangeRateResponse

--- app/src/main/java/com/quizocr/vitesse/data/api/model/ExchangeRateResponse.kt
+++ app/src/main/java/com/quizocr/vitesse/data/api/model/ExchangeRateResponse.kt
@@ -1,3 +1,13 @@
 package com.quizocr.vitesse.data.api.model
 
-data class ExchangeRateResponse()
+import com.google.gson.annotations.SerializedName
+
+data class ExchangeRateResponse(
+    @SerializedName("date")
+    val date: String?,
+
+    @SerializedName("eur")
+    val eur: Map<String, Double>?
+)
+
+
